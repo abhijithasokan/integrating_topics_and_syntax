754bd5ada65ee12362422fb622f00fd02b966a8d
anbilly19 <anoshbilimoria@gmail.com>
2023-09-24 17:24:26 +0200

    evaluations and jpegs

--- metrics.py
+++ metrics.py
@@ -6,7 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 PHI_Z_FILE = "phi_z.txt"
-dir_name = "output/0.02_0.02_0.02_0.02_10_8_4000_news_2000"
+PHI_C_FILE = "phi_c.txt"
+dir_name = "output/0.3_0.3_0.1_0.2_10_5_5000_news_5100"
 
 def plot(x,y,type):
     # Show graph
@@ -18,24 +19,28 @@ def plot(x,y,type):
     plt.legend()
     plt.xticks(rotation=45)
 
-    plt.savefig(f"{type}_n_iter_news.png")
+    plt.savefig(os.path.join(dir_name,f"{type}_n_iter_news.png"))
 
-def metrics(type,PHI_Z_FILE=PHI_Z_FILE, dir_name=dir_name):
-    def get_data():
-        dir_name = os.path.join(".","news_3000_test")
+def get_data(type):
+        if type=='test':
+            dir_name = os.path.join(".","news_3000_test")
+        else:
+            dir_name = os.path.join(".","news_5100")
         with open(os.path.join(dir_name, "documents.txt"), "r") as documents_file:
             data = documents_file.read().splitlines()
             documents = [[int(w) for w in d.split(' ')] for d in data if d != '']
         with open(os.path.join(dir_name, "vocab.txt"), "r") as vocab_file:
             vocab = vocab_file.read().split(' ')
         return vocab,documents
-    
-    vocab,docs = get_data()
+
+def metrics(type,PHI_Z_FILE=PHI_Z_FILE, dir_name=dir_name):
+    vocab,docs = get_data('train')
+    vocab_test,docs_test = get_data('test')
     list_of_docs = []
-    for doc in docs:
+    for doc in docs_test:
         doc_str_list = []
         for word_id in doc:
-            word = vocab[word_id]
+            word = vocab_test[word_id]
             doc_str_list.append(word)
         list_of_docs.append(doc_str_list)
 
@@ -47,6 +52,7 @@ def metrics(type,PHI_Z_FILE=PHI_Z_FILE, dir_name=dir_name):
     
     iter_list =[]
     coh_list = []
+    topics=[]
     for subdir, _, files in os.walk(dir_name):
         for file in files:
             if 'iter' in subdir and PHI_Z_FILE==file:
@@ -56,9 +62,18 @@ def metrics(type,PHI_Z_FILE=PHI_Z_FILE, dir_name=dir_name):
                 iter_list.append(iter_name)
                 cm = CoherenceModel(topics=topics,texts = list_of_docs,corpus=corpus, dictionary=id2word, coherence=type)
                 coh_list.append(cm.get_coherence())
+    pprint(topics)
     plot(iter_list,coh_list,type)
     
-
+def class_words():
+    subdir = os.path.join(dir_name,f"iter_3600")
+    vocab,docs = get_data('train')
+    class_word_counts = np.loadtxt(os.path.join(subdir, PHI_C_FILE))
+    def get_top_k_words_from_class( class_id: int, k: int):
+            top_k_words = np.argsort(class_word_counts[class_id])[:-k - 1:-1]
+            return [vocab[word_id] for word_id in top_k_words]
+    return [get_top_k_words_from_class(t, 10) for t in range(5)]
 
 if __name__ == "__main__":
-    metrics(type='c_npmi')
+    # metrics(type='c_v')
+    pprint(class_words())
--- pure_lda.py
+++ pure_lda.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def get_data():
-        dir_name = os.path.join(".","news_5100")
+        dir_name = os.path.join(".","data2000")
         with open(os.path.join(dir_name, "documents.txt"), "r") as documents_file:
             data = documents_file.read().splitlines()
             documents = [[int(w) for w in d.split(' ')] for d in data if d != '']
@@ -31,26 +31,56 @@ def pure_lda():
 # Term Document Frequency 
     corpus = [id2word.doc2bow(text) for text in texts]  
 
-    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+#     lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+#                                            id2word=id2word,
+#                                            num_topics=20, 
+#                                            random_state=100,
+#                                            update_every=1,
+#                                            chunksize=100,
+#                                            passes=10,
+#                                            alpha='auto',
+#                                            per_word_topics=True)
+
+#     for i, topic in lda_model.show_topics(num_topics=10, formatted=False):
+#         print('Topic {}: {}'.format(i, ', '.join([word[0] for word in topic])))
+# # Compute Perplexity
+#     print('\nPerplexity: ', lda_model.log_perplexity(corpus))  
+# a measure of how good the model is. lower the better.
+
+# Compute Coherence Score
+    # coherence_model_lda = CoherenceModel(model=lda_model,topn=1, texts=list_of_docs, dictionary=id2word, coherence='c_v')
+    # coherence_lda = coherence_model_lda.get_coherence_per_topic()
+    # print('\nCoherence Score: ', coherence_lda)
+    coherence_values = []
+    num_topics_list = []
+    for num_topics in range(1, 22, 5):
+        model=gensim.models.ldamodel.LdaModel(corpus=corpus,
                                            id2word=id2word,
-                                           num_topics=10, 
+                                           num_topics=num_topics, 
                                            random_state=100,
                                            update_every=1,
                                            chunksize=100,
                                            passes=10,
                                            alpha='auto',
                                            per_word_topics=True)
+        num_topics_list.append(f"{num_topics}")
+        coherencemodel = CoherenceModel(model=model,topn=10, texts=list_of_docs, dictionary=id2word, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
 
-    for i, topic in lda_model.show_topics(num_topics=10, formatted=False):
-        print('Topic {}: {}'.format(i, ', '.join([word[0] for word in topic])))
-# Compute Perplexity
-    print('\nPerplexity: ', lda_model.log_perplexity(corpus))  
-# a measure of how good the model is. lower the better.
+    return num_topics_list, coherence_values
+def plot(x,y,type):
+    # Show graph
+    plt.figure(figsize=(8,6))
+    plt.plot(x, y,label=type)
+    plt.title(f"Coherence score/ num topics ({type})")
+    plt.xlabel("n_topics")
+    plt.ylabel("Coherence score")
+    plt.legend()
+    plt.xticks(rotation=45)
 
-# Compute Coherence Score
-    coherence_model_lda = CoherenceModel(model=lda_model, texts=list_of_docs, dictionary=id2word, coherence='c_v')
-    coherence_lda = coherence_model_lda.get_coherence()
-    print('\nCoherence Score: ', coherence_lda)
-
+    plt.savefig(f"{type}_n_topics_news_pure.png")
 if __name__ == "__main__":
-    pure_lda()
+    x,y = pure_lda()
+    pprint(x)
+    pprint(y)
+    plot(x,y,'c_v')
